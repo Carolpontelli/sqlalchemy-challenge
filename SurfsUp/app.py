6b2be833452817769993f6697fadc42d3e9c4f1d
Carolpontelli <anacarolinapontelli@gmail.com>
2023-12-01 12:52:12 -0500

    Changes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,67 @@
-# Import the dependencies.
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+import datetime as dt
+import numpy as np
 
+# Criar uma aplicação Flask
+app = Flask(__name__)
 
+# Conectar ao banco de dados SQLite
+engine = create_engine("sqlite:///hawaii.sqlite")
 
-#################################################
-# Database Setup
-#################################################
+# Refletir o banco de dados em classes ORM
+Base = automap_base()
+Base.prepare(engine, reflect=True)
 
+# Referências para as tabelas
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
-# reflect an existing database into a new model
+# Criar uma sessão
+session = Session(engine)
 
-# reflect the tables
+# Definir rotas
 
+@app.route("/")
+def home():
+    return (
+        f"Bem-vindo à Climate Analysis API!<br/>"
+        f"Rotas Disponíveis:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;"
+    )
 
-# Save references to each table
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Calcular a data de um ano atrás a partir da última data no banco de dados
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
+    # Consultar dados de precipitação
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
 
-# Create our session (link) from Python to the DB
+    # Converter resultados para dicionário
+    precipitation_data = {date: prcp for date, prcp in results}
 
+    return jsonify(precipitation_data)
 
-#################################################
-# Flask Setup
-#################################################
+@app.route("/api/v1.0/stations")
+def stations():
+    # Consultar lista de estações
+    results = session.query(Station.station).all()
 
+    # Converter resultados para uma lista
+    station_list = list(np.ravel(results))
 
+    return jsonify(station_list)
 
+# Rotas semelhantes para outras análises...
 
-#################################################
-# Flask Routes
-#################################################
+if __name__ == "__main__":
+    app.run(debug=True)
